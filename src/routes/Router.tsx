d25863949de3414f446d4cf18c8d9e3c7eb978a4
Cara Steinberg <carasteinberg@Caras-Air.home>
2018-12-28 10:07:12 -0500

    Fixed ios to use env file variables

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { ConnectedRouter } from 'connected-react-router';
 import * as History from 'history';
 import { StyleSheet, Text, View } from 'react-native';
+import Config from 'react-native-config';
 import { Route } from 'react-router';
 // import {
 // } from '../screens';
@@ -10,11 +11,12 @@ export const Router = ({ history }: { history: History.History }) => (
   <ConnectedRouter history={history}>
     <View style={styles.container}>
       <Text>Hello World!</Text>
+      <Text>{Config.BUILD}  {Config.APP_ID} {Config.DISPLAY_NAME}</Text>
       {/* <Route exact={true} path={'/'} component={} /> */}
     </View>
   </ConnectedRouter>
 );
 
 const styles = StyleSheet.create({
-  container: { flex: 1 }
+  container: { flex: 1, paddingTop: 50 }
 });
